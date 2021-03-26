c6a755b3a70751004742b45c581c39b5e1bb1b2a
sombii <blackknightstd@gmail.com>
2021-03-26 15:08:35 +0545

    added profile links and changed some css for gradient

--- components/hero.js
+++ components/hero.js
@@ -2,13 +2,13 @@ import Particle from "./particle";
 
 export default function Hero() {
     return (
-        <section id="id" className="hero flex sm:mt-52 mt-20 sm:mb-32 flex-col lg:flex-row">
+        <section id="about" className="hero flex sm:mt-52 mt-20 sm:mb-32 flex-col lg:flex-row">
             <Particle/>
             <div className="">
                 <h1>
                     <span className="md:text-5xl text-4xl block z-10 relative">RAI,</span>
                     <span
-                        className="hero-text lg:text-8xl text-7xl py-3"
+                        className="hero-text lg:text-8xl text-7xl py-3 bg-gradient-to-r from-blue-800 via-purple-600 to-green-600"
                     >SANJOG</span>
                 </h1>
                 <div className="mt-20 font-chakra text-xl ml-4">
@@ -17,14 +17,14 @@ export default function Hero() {
                 </div>
                 <nav
                     className="flex text-lg items-center justify-start w-full pb-20 ml-4 mt-16 font-chakra">
-                    <a href="#works" className="mr-16 relative nav-item z-10">
+                    <a href="https://github.com/sombii/" target="_blank" className="mr-16 relative nav-item z-10">
                         <div>
                             <span className="relative z-10">github</span>
                             <span
                                 className="w-2 h-2 bg-blue-600 rounded-full inline-block absolute -right-6 bottom-1/2 transform translate-y-1/2 transition-all duration-20"/>
                         </div>
                     </a>
-                    <a href="#contact" className="relative nav-item z-10">
+                    <a href="https://linkedin.com/in/sombii" target="_blank" className="relative nav-item z-10">
                         <div>
                             <span className="relative z-10">linkedin</span>
                             <span
--- components/tags.js
+++ components/tags.js
@@ -1,8 +1,8 @@
 export default function Tags() {
     return (
         <>
-            <p className="absolute px-2 bg-blue-600 top-64 sm:-mx-24 -mx-20 left-1 transform -rotate-90">manjaro-xfce</p>
-            <p className="absolute px-2 bg-blue-600 top-3/4 sm:-mx-20 -mx-16 left-2 transform -rotate-90">phpstorm</p>
+            <p className="absolute px-2 font-chakra top-64 sm:-mx-20 -mx-16 left-1 transform -rotate-90">manjaro-xfce</p>
+            <p className="absolute px-2 font-chakra top-3/4 sm:-mx-16 -mx-12 left-1 transform -rotate-90">phpstorm</p>
         </>
     )
 }
